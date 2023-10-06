914beb37900bcf003fefc62f49c04ad03eebe5dd
Collin Brittain <collinbrittain@gmail.com>
2023-10-06 15:08:37 -0500

    Progress on logic

--- crates/simple-oracle/src/lib.rs
+++ crates/simple-oracle/src/lib.rs
@@ -1,21 +1,35 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::mpsc,
+};
 
 use eyre::Result;
-use ethers::abi::Address;
+use ethers::{
+    abi::Address,
+    providers::{Http, Provider},
+};
 use serde::{Deserialize, Serialize};
 
 mod querier;
+mod tx;
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct Config {
     pub ethereum_rpc_url: String,
     pub osmosis_grpc_url: String,
-    pub contract_mappings: HashMap<String, String>,
+    pub contract_map: HashMap<String, String>,
     pub signing_key_path: String,
+    pub assets: Vec<Asset>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Asset {
+    pub ethereum_contract: Address,
+    pub decimals: u32,
 }
 
 #[derive(Clone, Debug)]
-pub(crate) struct QuotePrice {
+pub struct QuotePrice {
     pub asset: Asset,
     pub value: u64,
     pub base: String,
@@ -23,16 +37,31 @@ pub(crate) struct QuotePrice {
     pub timestamp: u64,
 }
 
-#[derive(Clone, Debug)]
-pub(crate) struct Asset {
-    pub ethereum_contract: Address,
-    pub decimals: u32,
-}
-
 /// Should crate a channel, passing the sender to a thread running a [Querier], and the receiver
 /// to a thread running the tx submission logic. Tx thread will also need to construct the signing
 /// key from the key path in [Config]
 pub fn start(config: &Config) -> Result<()> {
-    todo!();
+    let (tx, rx) = mpsc::channel();
+
+    start_querier_thread(config, tx); 
+    start_tx_thread(config, rx);
+
+    Ok(())
+}
+
+pub fn start_querier_thread(config: &Config, tx: mpsc::Sender<QuotePrice>) -> Result<()> {
+    let config = config.clone();
+    
+    /// start thread
+
+    Ok(())
+}
+
+pub fn start_tx_thread(config: &Config, rx: mpsc::Receiver<QuotePrice>) -> Result<()> {
+    let config = config.clone();
+ 
+    /// start thread
+
+    Ok(())
 }
 
--- crates/simple-oracle/src/querier.rs
+++ crates/simple-oracle/src/querier.rs
@@ -1,4 +1,7 @@
-use std::sync::{Arc, mpsc};
+use std::{
+    collections::HashSet,
+    sync::{Arc, mpsc}
+};
 
 use ethers::{
     providers::{Http, Provider},
@@ -6,17 +9,30 @@ use ethers::{
 };
 use eyre::Result;
 
-use crate::{Asset, QuotePrice};
+use crate::{Asset, Config, QuotePrice};
 
 #[derive(Clone, Debug)]
 pub struct Querier {
     sender: mpsc::Sender<QuotePrice>,
     client: Arc<Provider<Http>>,
+    assets: HashSet<Asset>,
 }
 
 impl Querier {
-    pub(crate) fn new(sender: mpsc::Sender<QuotePrice>, client: Provider<Http>) -> Self {
-        Self { sender, client: Arc::new(client) }
+    pub(crate) fn new(config: &Config, sender: mpsc::Sender<QuotePrice>, client: Provider<Http>) -> Result<Self> {
+        let provider = Provider::<Http>::try_from(config.ethereum_rpc_url.as_str())?;
+
+        Ok(Self { sender, client: Arc::new(provider), assets: config.assets.clone() })
+    }
+
+    pub fn run(&self) -> Result<()> {
+        loop {
+            let asset = Asset::ETH;
+            let redemption_rate = self.get_redemption_rate(asset)?;
+            let quote = QuotePrice::new(asset, redemption_rate);
+
+            self.send(quote)?;
+        }
     }
 
     /// Calls previewRedeem(uint265) on the Cellar contract and returns a [QuotePrice]
